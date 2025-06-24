2d6c1f7d45c4acf1b1d83ef8c08a9385a49220f1
genstail24 <45913927+genstail24@users.noreply.github.com>
2025-06-24 13:31:49 +0700

    feat [backend(role-permission)]: apply permission middleware to routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,38 +23,67 @@ use Illuminate\Support\Facades\Route;
 Route::get('/hello-world', fn(Request $request) => 'Hello World');
 
 Route::prefix('auth')->group(function () {
+    // /auth
     Route::post('login', [AuthController::class, 'login'])->name('auth.login');
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('user', [AuthController::class, 'user'])->name('auth.user');
         Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
     });
 });
-
 Route::middleware('auth:sanctum')->group(function () {
     // /users
-    Route::apiResource('users', UserController::class);
-    Route::get('users/{user}/roles', [UserRoleController::class, 'index']);
-    Route::put('users/{user}/roles', [UserRoleController::class, 'sync']);
+    Route::prefix('users')->group(function () {
+        Route::middleware('permission:user.view')->get('/', [UserController::class, 'index']);
+        Route::middleware('permission:user.view')->get('{user}', [UserController::class, 'show']);
+        Route::middleware('permission:user.create')->post('/', [UserController::class, 'store']);
+        Route::middleware('permission:user.update')->put('{user}', [UserController::class, 'update']);
+        Route::middleware('permission:user.delete')->delete('{user}', [UserController::class, 'destroy']);
+
+        Route::middleware('permission:user.assign_role')->group(function () {
+            Route::get('{user}/roles', [UserRoleController::class, 'index']);
+            Route::put('{user}/roles', [UserRoleController::class, 'sync']);
+        });
+    });
 
     // /categories
-    Route::apiResource('categories', CategoryController::class);
+    Route::prefix('categories')->group(function () {
+        Route::middleware('permission:category.view')->get('/', [CategoryController::class, 'index']);
+        Route::middleware('permission:category.view')->get('{category}', [CategoryController::class, 'show']);
+        Route::middleware('permission:category.create')->post('/', [CategoryController::class, 'store']);
+        Route::middleware('permission:category.update')->put('{category}', [CategoryController::class, 'update']);
+        Route::middleware('permission:category.delete')->delete('{category}', [CategoryController::class, 'destroy']);
+    });
 
     // /reimbursements
     Route::prefix('reimbursements')->group(function () {
-        Route::apiResource('/', ReimbursementController::class)
-            ->parameters(['' => 'reimbursement'])
-            ->only(['index', 'show', 'destroy']);
-        Route::post('submission', [ReimbursementController::class, 'submission'])
-            ->name('reimbursements.submission');
-        Route::put('{reimbursement}/approval', [ReimbursementController::class, 'approval'])
-            ->name('reimbursements.approval');
+        Route::middleware('permission:reimbursement.view_all')->get('/', [ReimbursementController::class, 'index']);
+        Route::middleware('permission:reimbursement.view_single')->get('{reimbursement}', [ReimbursementController::class, 'show']);
+        Route::middleware('permission:reimbursement.delete')->delete('{reimbursement}', [ReimbursementController::class, 'destroy']);
+        Route::middleware('permission:reimbursement.create')->post('submission', [ReimbursementController::class, 'submission']);
+        Route::middleware('permission:reimbursement.approve')->put('{reimbursement}/approval', [ReimbursementController::class, 'approval']);
     });
 
     // /roles
-    Route::apiResource('roles', RoleController::class);
-    Route::get('roles/{role}/permissions', [RolePermissionController::class, 'index']);
-    Route::put('roles/{role}/permissions', [RolePermissionController::class, 'sync']);
+    Route::prefix('roles')->group(function () {
+        Route::middleware('permission:role.view')->get('/', [RoleController::class, 'index']);
+        Route::middleware('permission:role.view')->get('{role}', [RoleController::class, 'show']);
+        Route::middleware('permission:role.create')->post('/', [RoleController::class, 'store']);
+        Route::middleware('permission:role.update')->put('{role}', [RoleController::class, 'update']);
+        Route::middleware('permission:role.delete')->delete('{role}', [RoleController::class, 'destroy']);
+
+        Route::middleware('permission:role.sync_permissions')->group(function () {
+            Route::get('{role}/permissions', [RolePermissionController::class, 'index']);
+            Route::put('{role}/permissions', [RolePermissionController::class, 'sync']);
+        });
+    });
 
     // /permissions
-    Route::apiResource('permissions', PermissionController::class);
+    Route::prefix('permissions')->group(function () {
+        Route::middleware('permission:permission.view')->get('/', [PermissionController::class, 'index']);
+        Route::middleware('permission:permission.view')->get('{permission}', [PermissionController::class, 'show']);
+        Route::middleware('permission:permission.create')->post('/', [PermissionController::class, 'store']);
+        Route::middleware('permission:permission.update')->put('{permission}', [PermissionController::class, 'update']);
+        Route::middleware('permission:permission.delete')->delete('{permission}', [PermissionController::class, 'destroy']);
+    });
+
 });
